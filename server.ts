1ce12b235e6c8f88569e27c4f0c08eb96de93d42
William Dunston <wedunsto@aggies.ncat.edu>
2026-08-27 10:44:53 -0400

    Installed Cors

--- server.ts
+++ server.ts
@@ -1,11 +1,18 @@
 import "dotenv/config";
+import cors from "cors";
 const express = require('express');
+import { allowedOrigins } from "./src/middleware/cors.middleware";
 import { authenticationRouter } from "./src/routes/authentication.routes";
 
 const app = express();
 const PORT = process.env.SERVER_PORT || 3000;
 
 app.use(express.json());
+
+// Tell the browser which origins are permitted
+app.use(cors({
+    origin: allowedOrigins, credentials: true
+}));
 
 app.use("/yourfinance", authenticationRouter);
 
